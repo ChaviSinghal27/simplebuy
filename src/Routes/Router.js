04bfae21697c8c9add688fada438c8f300c54f89
ChaviSinghal27 <108614142+ChaviSinghal27@users.noreply.github.com>
2023-09-16 00:59:04 +0530

    add: Router

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,14 +1,14 @@
 import {Routes , Route} from "react-router-dom"
 import { Home } from "../pages/home/Home"
 import { Products } from "../pages/Productpage/Products"
-import { Cart } from "../pages/cart"
-import { Wishlist } from "../pages/wishlist"
+import { Cart } from "../pages/Cart"
+import { Wishlist } from "../pages/Wishlist"
 
 export function Router () {
     return (
         <div>
             <Routes>
-<Route path="/Home" element={<Home/>} />
+<Route path="/"  exact element={<Home/>} />
 <Route path="/Product" element = {<Products/>} />
 <Route path = "/Cart" element = {<Cart/>} />
 <Route path = "/Wishlist" element = {<Wishlist/>} />
--- src/utilities/data.js
+++ src/utilities/data.js
@@ -1,130 +1,121 @@
 export const productData = {
-    productList: [
-      {
-        id: " 1",
-        name: "",
-        price: "1000",
-        category: "Bags",
-        gender: "women",
-        URL:
-          "https://images.pexels.com/photos/11124989/pexels-photo-11124989.jpeg",
-        discount: "15%"
-      },
-      {
-        id: " 2",
-        name: "",
-        price: "2000",
-        category: "Bags",
-        gender: "Women",
-        URL: "https://www.nat-nin.com/cdn/shop/products/DANY_CROCPRALINE_V1.webp",
-        discount: "25%"
-      },
-      {
-        id: "3",
-        name: "",
-        price: "3000",
-        category: "Bags",
-        gender: "Women",
-        URL: "https://www.nat-nin.com/cdn/shop/products/NOELLA_MYRTILLE_V1.webp",
-        discount: "35%"
-      },
-      {
-        id: " 4",
-        name: "Shoulder Strap",
-        price: "4000",
-        category: "Accesories",
-        gender: "Women",
-        URL:
-          "https://www.nat-nin.com/cdn/shop/products/BANDOULIERERECYCLE_NOIR_SILVER_3700723357081_P3_452b54f0-ac1b-4172-b6a4-e3b6cc0803d6.webp",
-        discount: "45%"
-      },
-      {
-        id: "5",
-        name: "Card Holder",
-        price: "1500",
-        category: "Accesories",
-        gender: "Women",
-        URL: "https://www.nat-nin.com/cdn/shop/files/ALIX_CACAO_V1.webp",
-        discount: "15%"
-      },
-      {
-        id: " 6",
-        name: "",
-        price: "2000",
-        category: "Shoes",
-        gender: "Women",
-        URL:
-          "https://www.nat-nin.com/cdn/shop/products/SAVANNAH_NOIR_38_3700723356671_V2.webp",
-        discount: "55%"
-      },
-      {
-        id: "7",
-        name: "",
-        price: "4500",
-        category: "Shoes",
-        gender: "Women",
-        URL:
-          "https://www.nat-nin.com/cdn/shop/products/MALDIVES_ROUGE_AMOUR_36_3700723363457_V1_2c15da60-06b8-46ad-8e3d-2763ccbd66d2.webp ",
-        discount: "25%"
-      },
-      {
-        id: " 8",
-        name: "",
-        price: "2500",
-        category: "Bags",
-        gender: "Men",
-        URL:
-          "https://www.nat-nin.com/cdn/shop/products/BASILE_NOIR_3700723364027_V2.webp",
-        discount: "15%"
-      },
-      {
-        id: "9",
-        name: "",
-        price: "1500",
-        category: "Bags",
-        gender: "Men",
-        URL:
-          "https://www.nat-nin.com/cdn/shop/products/BRUCE_NOIR_3700723313049_V1.webp",
-        discount: "35%"
-      },
-      {
-        id: "10",
-        name: "",
-        price: "3000",
-        category: "Accesories",
-        gender: "Men",
-        URL:
-          "https://www.nat-nin.com/cdn/shop/products/ALEXIS_KAKI_3700723356817_V1.webp",
-        discount: "45%"
-      },
-      {
-        id: " 11",
-        name: "",
-        price: "2000",
-        category: "Accesories",
-        gender: "Men",
-        URL:
-          "https://www.nat-nin.com/cdn/shop/products/BEN_NOIR_3700723361545_P1.webp",
-        discount: "15%"
-      },
-      {
-        id: "12",
-        name: "",
-        price: "5000",
-        category: "Shoes",
-        gender: "Men",
-        URL: "https://images.pexels.com/photos/292999/pexels-photo-292999.jpeg",
-        discount: "55%"
-      },
-      {
-        id: "13",
-        name: "",
-        price: "5500",
-        category: "Shoes",
-        gender: "Men",
-        URL: "https://images.pexels.com/photos/3261068/pexels-photo-3261068.jpeg",
-        discount: "25%"
-      }
-    ]
-  };
-  
+  productList: [
+    {
+      id: "1",
+      name: "Green Bag",
+      price: 1000,
+      category: "Bags",
+      gender: "Women",
+      URL: "https://images.pexels.com/photos/11124989/pexels-photo-11124989.jpeg",
+      discount: "15%",
+    },
+    {
+      id: "2",
+      name: "Barbie Purse",
+      price: 2000,
+      category: "Bags",
+      gender: "Women",
+      URL: "https://www.nat-nin.com/cdn/shop/products/DANY_CROCPRALINE_V1.webp",
+      discount: "25%",
+    },
+    {
+      id: "3",
+      name: "Purple Shoulderbag",
+      price: 3000,
+      category: "Bags",
+      gender: "Women",
+      URL: "https://www.nat-nin.com/cdn/shop/products/NOELLA_MYRTILLE_V1.webp",
+      discount: "35%",
+    },
+    {
+      id: "4",
+      name: "Shoulder Strap",
+      price: 4000,
+      category: "Accesories",
+      gender: "Women",
+      URL: "https://www.nat-nin.com/cdn/shop/products/BANDOULIERERECYCLE_NOIR_SILVER_3700723357081_P3_452b54f0-ac1b-4172-b6a4-e3b6cc0803d6.webp",
+      discount: "45%",
+    },
+    {
+      id: "5",
+      name: "Card Holder",
+      price: 1500,
+      category: "Accesories",
+      gender: "Women",
+      URL: "https://www.nat-nin.com/cdn/shop/files/ALIX_CACAO_V1.webp",
+      discount: "15%",
+    },
+    {
+      id: "6",
+      name: "Black Boots",
+      price: 2000,
+      category: "Shoes",
+      gender: "Women",
+      URL: "https://www.nat-nin.com/cdn/shop/products/SAVANNAH_NOIR_38_3700723356671_V2.webp",
+      discount: "55%",
+    },
+    {
+      id: "7",
+      name: "Red Hot Heels",
+      price: 4500,
+      category: "Shoes",
+      gender: "Women",
+      URL: "https://www.nat-nin.com/cdn/shop/products/MALDIVES_ROUGE_AMOUR_36_3700723363457_V1_2c15da60-06b8-46ad-8e3d-2763ccbd66d2.webp ",
+      discount: "25%",
+    },
+    {
+      id: "8",
+      name: "Fanny Bag",
+      price: 2500,
+      category: "Bags",
+      gender: "Men",
+      URL: "https://www.nat-nin.com/cdn/shop/products/BASILE_NOIR_3700723364027_V2.webp",
+      discount: "15%",
+    },
+    {
+      id: "9",
+      name: "Office Bag",
+      price: 1500,
+      category: "Bags",
+      gender: "Men",
+      URL: "https://www.nat-nin.com/cdn/shop/products/BRUCE_NOIR_3700723313049_V1.webp",
+      discount: "35%",
+    },
+    {
+      id: "10",
+      name: "men's Purse",
+      price: 3000,
+      category: "Accesories",
+      gender: "Men",
+      URL: "https://www.nat-nin.com/cdn/shop/products/ALEXIS_KAKI_3700723356817_V1.webp",
+      discount: "45%",
+    },
+    {
+      id: "11",
+      name: "Office kit",
+      price: 2000,
+      category: "Accesories",
+      gender: "Men",
+      URL: "https://www.nat-nin.com/cdn/shop/products/BEN_NOIR_3700723361545_P1.webp",
+      discount: "15%",
+    },
+    {
+      id: "12",
+      name: "Black formals ",
+      price: 5000,
+      category: "Shoes",
+      gender: "Men",
+      URL: "https://images.pexels.com/photos/292999/pexels-photo-292999.jpeg",
+      discount: "55%",
+    },
+    {
+      id: "13",
+      name: "Men's Sneakers",
+      price: 5500,
+      category: "Shoes",
+      gender: "Men",
+      URL: "https://images.pexels.com/photos/3261068/pexels-photo-3261068.jpeg",
+      discount: "25%",
+    },
+  ],
+};
