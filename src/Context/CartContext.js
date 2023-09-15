c9150bd1b18cee13abc05e645c63b7d17d6c3e29
ChaviSinghal27 <108614142+ChaviSinghal27@users.noreply.github.com>
2023-09-16 01:02:16 +0530

    add: cartContext

---
+++ src/Context/CartContext.js
@@ -0,0 +1,38 @@
+import { createContext, useContext , useReducer, useState } from "react";
+
+
+const CartContext = createContext();
+
+ function handleCartItems (state , action) {
+    switch (action.type) {
+        case "HANDLE_CART":
+         return (state = 
+            {
+        
+        productsInCart: state.productsInCart.includes(action.payload) 
+        ? state.productsInCart.filter((id)=> id !== action.payload) 
+        : [...state.productsInCart , action.payload]
+              
+    }) ;
+        
+          default:
+            return state;
+    }
+}
+
+
+const CartProvider = ({children}) => {
+
+    const [state, dispatch] = useReducer(handleCartItems , {
+        productsInCart: []
+    });
+    return (
+        <CartContext.Provider value = {{state, dispatch}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+const useCartContext = () => useContext(CartContext);  
+
+export {CartProvider , useCartContext , handleCartItems};
