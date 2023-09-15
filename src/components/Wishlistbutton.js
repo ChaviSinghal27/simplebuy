dd3516a07b09fbf9b4fdd448ceda4f5c9be7d43c
ChaviSinghal27 <108614142+ChaviSinghal27@users.noreply.github.com>
2023-09-16 01:02:31 +0530

    add:Wishlistbutton

--- src/components/Wishlistbutton.js
+++ src/components/Wishlistbutton.js
@@ -1,11 +1,26 @@
+import { useState } from "react"
+import { useWishlistContext } from "../Context/WishlistContext";
+import { productData } from "../utilities/data";
 
 
-export function Wishlistbutton () {
+
+
+
+export function Wishlistbutton ({productId}) {
+
+    const {wishlistItem , setWishlistItem} = useWishlistContext();
+ 
+     
+const wishlistHandler = () => {
+wishlistItem.includes(productId) ? setWishlistItem(wishlistItem.filter((id)=> id !== productId )) : setWishlistItem([...wishlistItem ,productId])
+
+}
     return (
-       
-         <div className="wishlist-icon">
-        <i class="fa-solid fa-heart"></i>
-         </div>
+    <div className="wishlist-icon" onClick={wishlistHandler} >
+        <i class="fa-solid fa-heart" style={ {color: wishlistItem.includes(productId) ?  "red" : "black"}} ></i>
+        
+        </div>
         
     )
+
 }
